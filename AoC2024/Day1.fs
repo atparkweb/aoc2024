5adb4364d95d116b4a0e6a4a160ecdafc7ee8766
Andy Park <atpark333@gmail.com>
2024-12-01 22:30:13 +0900

    Solve day 1-1

--- AoC2024/Day1.fs
+++ AoC2024/Day1.fs
@@ -1,8 +1,24 @@
 namespace AoC24
 
-module public Day1 =
-    let greet name =
-        printfn $"Hello {name}. Nice to meet you!"
+open System
 
-    let add x y =
-        x + y
+module public Day1 =
+    let private sortLists x y =
+        ((List.sort x), (List.sort y))
+
+    let private totalDifferences list1 list2 =
+        let rec loop left right total =
+            match left, right with
+            | [], _
+            | _, [] -> total
+            | h1::t1, h2::t2 ->
+                loop t1 t2 (total + abs(h2 - h1))
+        (loop list1 list2 0)
+        
+    let solve x y =
+        if List.length x <> List.length y
+        then raise (ArgumentException "The input lists must be the same length")
+        else
+            sortLists x y
+            |> fun (sorted1, sorted2) -> totalDifferences sorted1 sorted2
+        
