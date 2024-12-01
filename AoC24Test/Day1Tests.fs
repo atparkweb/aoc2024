02ded579ced5fcebc6d7a4606e4da251b0106461
Andy Park <atpark333@gmail.com>
2024-12-01 22:30:13 +0900

    Solve day 1-1

--- AoC24Test/Day1Tests.fs
+++ AoC24Test/Day1Tests.fs
@@ -1,9 +1,20 @@
 module Day1Tests
 
-open System
 open AoC24
 open Xunit
 
 [<Fact>]
-let ``Can add two numbers`` () =
-    Assert.Equal(4, Day1.add 2 2)
+let ``Returns zero when both lists are empty`` () =
+    Assert.Equal(0, Day1.solve [] [])
+    
+[<Fact>]
+let ``Returns zero when lists are unequal sizes`` () =
+    Assert.Equal(0, Day1.solve [1; 2; 3] [4; 5])
+    
+[<Fact>]
+let ``Can get total difference between lists that contain 1 item each`` () =
+    Assert.Equal(1, Day1.solve [2] [3])
+    
+[<Fact>]
+let ``Can get total difference between pairs of taken from two lists`` () =
+    Assert.Equal(11, Day1.solve [3; 4; 2; 1; 3; 3 ] [4; 3; 5; 3; 9; 3])
