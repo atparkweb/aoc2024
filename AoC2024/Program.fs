95a6ee5cbd00317a579f961b712956ff4163f231
Andy Park <atpark333@gmail.com>
2024-12-01 22:30:31 +0900

    Update main to run day 1-1

--- AoC2024/Program.fs
+++ AoC2024/Program.fs
@@ -1,7 +1,10 @@
 ﻿namespace AoC24
 
+open AoC2024.input.Day1_input
+
 module Program =
     [<EntryPoint>]
     let main _args =
-        Day1.greet "Andy"
+        let answer = Day1.solve input1 input2
+        printfn $"The answer is: {answer}"
         0
