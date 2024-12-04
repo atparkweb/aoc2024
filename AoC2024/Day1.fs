4303386ae15fa1fc696d53ffc53106f6e7f53ef7
Andy Park <atpark333@gmail.com>
2024-12-04 21:53:17 +0900

    Refactor day 1-1

--- AoC2024/Day1.fs
+++ AoC2024/Day1.fs
@@ -1,7 +1,6 @@
 namespace AoC24
 
 open System
-open System.IO
 
 module public Day1 =
     let private sortLists x y =
@@ -10,16 +9,16 @@ module public Day1 =
     let private totalDifferences list1 list2 =
         let rec loop left right total =
             match left, right with
-            | [], _
+            | [], _ -> total
             | _, [] -> total
             | h1::t1, h2::t2 ->
                 loop t1 t2 (total + abs(h2 - h1))
-        (loop list1 list2 0)
+        if List.length list1 <> List.length list2 then
+            raise (ArgumentException "The input lists must be the same length")
+        else
+            (loop list1 list2 0)
         
     let solve (x: int list, y: int list) =
-        if List.length x <> List.length y
-        then raise (ArgumentException "The input lists must be the same length")
-        else
-            sortLists x y
-            |> fun (sorted1, sorted2) -> totalDifferences sorted1 sorted2
+        let (sorted1, sorted2) = sortLists x y
+        totalDifferences sorted1 sorted2
         
