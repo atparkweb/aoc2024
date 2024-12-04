0726c1ec4a049bf52d00c84340453b9096ae69f8
Andy Park <atpark333@gmail.com>
2024-12-04 21:29:25 +0900

    Move input reading functions to common module

--- AoC2024/CommonUtils.fs
+++ AoC2024/CommonUtils.fs
@@ -8,5 +8,34 @@ module CommonUtils =
         let baseDir = AppDomain.CurrentDomain.BaseDirectory
         Directory.GetParent(baseDir).Parent.Parent.Parent.FullName
         
-    let getAbsoluteContentPath filePath =
+    let private getAbsoluteContentPath filePath =
         Path.Combine(getProjectRootDirectory(), filePath)
+
+    (** <summary>
+    Converts a text file containing two columns to two lists.
+    Columns are delimited by spaces.
+    </summary>
+    <param name="filePath">The relative path to the text file. Relative to project directory</param>
+    <returns>Two lists of equal length. Each item being a line in each column.</returns>
+    *)
+    let columnsFromFile filePath =
+        let lines = File.ReadAllLines(getAbsoluteContentPath(filePath))
+        let list1, list2 =
+            lines
+            |> Seq.fold (fun (left, right) line ->
+                let parts = line.Split([|' '|], StringSplitOptions.RemoveEmptyEntries) |> List.ofArray
+                let leftPart = int parts.[0]
+                let rightPart = int parts.[1]
+                (leftPart :: left, rightPart :: right)) ([], [])
+        (List.rev list1, List.rev list2)
+        
+    (** <summary>
+    Converts a text file containing integers on each line to an two-dimensional array of integers.
+    </summary>
+    <param name="filePath">The relative path to the text file. Relative to the project directory</param>
+    <returns>An array of each line from the file. Each line converted to an array of integers.
+    *)
+    let numericalDataFromFile filePath =
+        getAbsoluteContentPath(filePath)
+        |> File.ReadAllLines
+        |> Array.map (fun line -> (Array.map Int32.Parse (line.Split " ")))
--- AoC2024/Program.fs
+++ AoC2024/Program.fs
@@ -6,26 +6,14 @@ open System.IO
 module Program =
     [<EntryPoint>]
     let main _args =
-        let listsFromFile filePath=
-            let lines = File.ReadAllLines(filePath)
-            let list1, list2 =
-                lines
-                |> Seq.fold (fun (left, right) line ->
-                    let parts = line.Split([|' '|], StringSplitOptions.RemoveEmptyEntries) |> List.ofArray
-                    let leftPart = int parts.[0]
-                    let rightPart = int parts.[1]
-                    (leftPart :: left, rightPart :: right)) ([], [])
-            (List.rev list1, List.rev list2)
+        // Solve Day 1-1
+        let input1 = CommonUtils.columnsFromFile "input/day1.txt"
+        let day1 = (1, Day1.solve input1)
         
-
-        let input1 = listsFromFile (CommonUtils.getAbsoluteContentPath "input/day1.txt")
-        let answer1 = Day1.solve input1
-        printfn $"Day 1 answer is: {answer1}"
+        // Solve Day 2-1
+        let input2 = CommonUtils.numericalDataFromFile "input/day2.txt"
+        let day2 = (2, Day2.solve input2)
         
-        let input2 =
-            CommonUtils.getAbsoluteContentPath("input/day2.txt")
-            |> File.ReadAllLines
-            |> Array.map (fun line -> (Array.map Int32.Parse (line.Split " ")))
-        let answer2 = Day2.solve input2
-        printfn $"Day 2 answer is: {answer2}"
+        // Print out the solutions to the console
+        [|day1; day2|] |> Array.iter (fun (i, a) -> printfn $"Day {i} answer: {a}")
         0
