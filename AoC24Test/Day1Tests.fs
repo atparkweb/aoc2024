77ef09facf66468406c4c9698c9beb9e9fa79542
Andy Park <atpark333@gmail.com>
2024-12-04 21:08:08 +0900

    Update unit tests

--- AoC24Test/Day1Tests.fs
+++ AoC24Test/Day1Tests.fs
@@ -1,20 +1,21 @@
 module Day1Tests
 
+open System
 open AoC24
 open Xunit
 
 [<Fact>]
 let ``Returns zero when both lists are empty`` () =
-    Assert.Equal(0, Day1.solve [] [])
+    Assert.Equal(0, Day1.solve ([], []))
     
 [<Fact>]
-let ``Returns zero when lists are unequal sizes`` () =
-    Assert.Equal(0, Day1.solve [1; 2; 3] [4; 5])
+let ``Raises an argument exception when lists are different lengths`` () =
+    Assert.Throws<ArgumentException>(fun () -> Day1.solve ([1; 2; 3], [4; 5]) |> ignore)
     
 [<Fact>]
 let ``Can get total difference between lists that contain 1 item each`` () =
-    Assert.Equal(1, Day1.solve [2] [3])
+    Assert.Equal(1, Day1.solve ([2], [3]))
     
 [<Fact>]
 let ``Can get total difference between pairs of taken from two lists`` () =
-    Assert.Equal(11, Day1.solve [3; 4; 2; 1; 3; 3 ] [4; 3; 5; 3; 9; 3])
+    Assert.Equal(11, Day1.solve ([3; 4; 2; 1; 3; 3 ], [4; 3; 5; 3; 9; 3]))
--- AoC24Test/Day2Tests.fs
+++ AoC24Test/Day2Tests.fs
@@ -5,4 +5,5 @@ open Xunit
 
 [<Fact>]
 let ``Finds the correct number of safe reports`` () =
-    Assert.Equal(0, Day2.solve)
+    Assert.Equal(1, Day2.solve [|[|1; 2; 3; 4|]|])
+    Assert.Equal(0, Day2.solve [|[|2; 4; 1; 5|]|])
