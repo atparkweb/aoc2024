4112cb6eb3f602d1aeaab1a2d17aae84eef3f399
Andy Park <atpark333@gmail.com>
2024-12-05 09:15:02 +0900

    Add day 3-1 files and output

---
+++ AoC2024/Day3.fs
@@ -0,0 +1,6 @@
+namespace AoC24
+
+module public Day3 =
+    let solve input =
+        // TODO: Solve day 3-1
+        0
--- AoC2024/Program.fs
+++ AoC2024/Program.fs
@@ -11,6 +11,10 @@ module Program =
         let input2 = CommonUtils.numericalDataFromFile "input/day2.txt"
         let day2 = (2, Day2.solve input2)
         
+        // Solve Day 3-1
+        let input3 = CommonUtils.bytesFromFile "input/day3.txt"
+        let day3 = (3, Day3.solve input3)
+        
         // Print out the solutions to the console
-        [| day1; day2; |] |> Array.iter (fun (i, a) -> printfn $"Day {i} answer: {a}")
+        [| day1; day2; day3; |] |> Array.iter (fun (i, a) -> printfn $"Day {i} answer: {a}")
         0
