7e4fae583943f1c29b8b25b7f6729113b39919d9
Andy Park <atpark333@gmail.com>
2024-12-08 19:09:06 +0900

    Solve day 3-1 Cheated by using regular expressions.

--- AoC2024/Day3.fs
+++ AoC2024/Day3.fs
@@ -1,6 +1,15 @@
 namespace AoC24
 
+open System
+open System.Text.RegularExpressions
+
 module public Day3 =
     let solve input =
-        // TODO: Solve day 3-1
-        0
+        let pattern = Regex(@"mul\((\d+),(\d+)\)")
+        let matches = pattern.Matches(input)
+        let mutable total = 0
+        for m in matches do
+            let product =
+                Int32.Parse(m.Groups.[1].Value) * Int32.Parse(m.Groups.[2].Value)
+            total <- total + product
+        total
